661ea5a74fff0deedc96ce871d7686f0aeebbd2e
Emiliano Montes <louieliet2003@gmail.com>
2024-12-27 14:32:20 -0600

    remove animation on aboutme

--- app/pages/AboutMe.tsx
+++ app/pages/AboutMe.tsx
@@ -1,21 +1,11 @@
 "use client";
-import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
 import AboutWeb from "@/app/pages/About/AboutWeb";
 import AboutMobile from "@/app/pages/About/AboutMobile";
 
 const AboutMe = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-    });
-  }, []);
-
   return (
-    <div className="flex min-h-screen" data-aos="fade">
+    <div className="flex min-h-screen">
       <div className="text-start w-full px-20">
         <h1 className="text-8xl text-white font-extrabold md:text-6xl py-5 md:py-10 md:w-full">
           [01] ¿Qué hago?
